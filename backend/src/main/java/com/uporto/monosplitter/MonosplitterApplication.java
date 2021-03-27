8b7a3300ee52b9a1b93f3d25b98bd89593224558
ruben.jesus <ruben.x.c.jesus@gmail.com>
2021-03-27 15:49:12 +0000

    backend - first commit

---
+++ backend/src/main/java/com/uporto/monosplitter/MonosplitterApplication.java
@@ -0,0 +1,12 @@
+package com.uporto.monosplitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonosplitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonosplitterApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/uporto/monosplitter/MonosplitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uporto.monosplitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonosplitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
